d54099d6e6dd751eeb4ca0665238027cc9364f98
nevrome <clemens@nevrome.de>
2018-07-17 14:44:34 +0200

    Test: Besteht das citeproc Problem noch?

--- _build.sh
+++ _build.sh
@@ -1,6 +1,3 @@
 #!/usr/bin/env Rscript
 
-# remove bibliography to make a build on travis possible (citeproc error)
-system("sed -i '/bibliography:/d' ./index.Rmd")
-
 bookdown::render_book("index.Rmd")
