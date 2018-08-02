af5de3fed0211a4cb5921db76257c3dcb66ee3d9
nevrome <clemens@nevrome.de>
2018-08-02 23:37:54 +0200

    Versuch: travis beibringen neomod_analysis zu clonen

---
+++ _clone_neomod_analysis.sh
@@ -0,0 +1,5 @@
+cd ..
+
+git clone git@github.com:nevrome/neomod_analysis.git
+
+cd neomod_textdev
--- _deploy.sh
+++ _deploy.sh
@@ -1,7 +1,3 @@
-# configure your name and email if you have not done so
-git config --global user.email "clemens@nevrome.de"
-git config --global user.name "Clemens Schmid"
-
 # clone the repository to the book-output directory
 git clone -b document \
   https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git \
