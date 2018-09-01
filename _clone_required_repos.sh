16507fa3791e14f968219b5a5738d6bab6dd58f8
nevrome <clemens@nevrome.de>
2018-09-02 00:15:49 +0200

    Versuch eine Abbildung aus dem neiman1995 Repo einzubinden

--- _clone_required_repos.sh
+++ _clone_required_repos.sh
@@ -4,5 +4,6 @@ git config --global user.name "Clemens Schmid"
 cd ..
 
 git clone https://${GITHUB_PAT}@github.com/nevrome/neomod_analysis.git
+git clone https://${GITHUB_PAT}@github.com/nevrome/neiman1995.git
 
 cd neomod_textdev
